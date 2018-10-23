9a49e566f251320265cab0adbb3d3e659a4f74bb
Lukas Pirner <lpirner@stud.hs-heilbronn.de>
2018-10-23 22:01:48 +0200

    first implementation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,74 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Label RproZ;
+
+    @FXML
+    private TextField rho;
+
+    @FXML
+    private TextField faktor;
+
+    @FXML
+    private TextField anzmae;
+
+    @FXML
+    private TextField laengeTwin;
+
+    @FXML
+    private TextField sonstLae;
+
+    @FXML
+    private TextField trackWidth;
+
+    @FXML
+    private TextField copperThickness;
+
+    @FXML
+    private TextField akkuV;
+
+    @FXML
+    private TextField watth;
+
+    @FXML
+    private Label Rges;
+
+
+    private double lenghtproMeander;
+    private double anzMeand;
+    private double sonstleange;
+    private double fakt;
+
+    private double trackWidthC;
+    private double copperThicknessC;
+
+    private double rhoC;
+
+
+    public void CalcAll(){
+
+
+        lenghtproMeander = Double.parseDouble(laengeTwin.getText());
+        anzMeand = Double.parseDouble(anzmae.getText());
+        sonstleange= Double.parseDouble(sonstLae.getText());
+        fakt = Double.parseDouble(faktor.getText());
+        rhoC = Double.parseDouble(rho.getText());
+
+        trackWidthC = Double.parseDouble(trackWidth.getText());
+        copperThicknessC = Double.parseDouble(copperThickness.getText());
+
+        double Rresult = (rhoC*((10*fakt)/(trackWidthC*copperThicknessC)));
+
+
+
+        RproZ.setText(String.valueOf(Rresult));
+
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        primaryStage.setTitle("GloveResistor Calculator");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1102, 570));
         primaryStage.show();
     }
 
